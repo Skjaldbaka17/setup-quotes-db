a1f78d79b10ab91c6670be11d5ed01ac99aa6046
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-05-24 19:59:28 +0000

    Made sql files for the new setup

--- sql/initQueries.sql
+++ sql/initQueries.sql
@@ -1,3 +1,12 @@
-CREATE INDEX index_authors_on_name ON authors USING gin(to_tsvector(name));
-CREATE INDEX index_quotes_on_quote ON quotes USING gin(to_tsvector(quote));
-CREATE EXTENSION pg_trgm; --https://www.freecodecamp.org/news/fuzzy-string-matching-with-postgresql/
+ALTER TABLE quotes ADD COLUMN tsv tsvector; --Create tsvector column
+UPDATE quotes SET tsv =
+    setweight(to_tsvector('english', quote), 'A');
+
+ALTER TABLE authors ADD COLUMN tsv tsvector; --Create tsvector column
+UPDATE authors SET tsv =
+    setweight(to_tsvector('english', name), 'A');
+
+CREATE INDEX index_authors_on_name ON authors USING gin(tsv);
+CREATE INDEX index_quotes_on_quote ON quotes USING gin(tsv);
+CREATE INDEX index_authors_on_sim_name on authors using gin()
+CREATE EXTENSION pg_trgm; --https://www.freecodecamp.org/news/fuzzy-string-matching-with-postgresql/ needed for SIMILARITY()-function
--- sql/searchView.sql
+++ sql/searchView.sql
@@ -3,7 +3,10 @@ select authors.id as authorid,
        authors.name,
        quotes.id as quoteid,
        quotes.quote as quote,
-       quotes.isicelandic as isicelandic
+       quotes.isicelandic as isicelandic,
+       authors.tsv || quotes.tsv  as tsv,
+       authors.tsv as nametsv,
+       quotes.tsv as quotetsv
 from authors
    inner join quotes
       on authors.id = quotes.authorid;
