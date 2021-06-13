6a6b704c52099e6a88197862dbabea3e05d9bd80
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-06-13 15:14:37 +0000

    Added apikey hash to users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -2,6 +2,7 @@ CREATE TABLE users(
    id SERIAL PRIMARY KEY,
    email varchar not null unique,
    name VARCHAR NOT NULL UNIQUE,
+   apikeyhash varchar not null unique,
    passwordhash text not null,
    email varchar,
    tier varchar not null default 'free',
