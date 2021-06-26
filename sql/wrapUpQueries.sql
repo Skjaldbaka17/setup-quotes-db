42d1d9fedb9abad77476cc43caeaf55ca04f682b
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-06-26 17:32:23 +0000

    Added some indexes to quotes table for faster queries

--- sql/wrapUpQueries.sql
+++ sql/wrapUpQueries.sql
@@ -2,4 +2,6 @@ UPDATE authors SET tsv = setweight(to_tsvector('english', name), 'A');
 UPDATE quotes SET tsv = setweight(to_tsvector('english', quote), 'A');
 CREATE INDEX if not exists index_authors_on_name ON authors USING gin(tsv);
 CREATE INDEX if not exists index_quotes_on_quote ON quotes USING gin(tsv);
+CREATE INDEX if not exists index_quotes_on_author_id ON quotes(author_id);
+CREATE INDEX if not exists index_quotes_on_count ON quotes(count);
 CREATE EXTENSION if not exists pg_trgm;
