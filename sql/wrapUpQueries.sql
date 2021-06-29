04a02a785f9e33aed3855f3813e067054488f656
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-06-29 14:38:07 +0000

    Removed index from mat-view because of postgres error

--- sql/wrapUpQueries.sql
+++ sql/wrapUpQueries.sql
@@ -4,5 +4,4 @@ CREATE INDEX if not exists index_authors_on_name ON authors USING gin(tsv);
 CREATE INDEX if not exists index_quotes_on_quote ON quotes USING gin(tsv);
 CREATE INDEX if not exists index_quotes_on_author_id ON quotes(author_id);
 CREATE INDEX if not exists index_quotes_on_count ON quotes(count);
-CREATE INDEX if not exists index_view_on_tsv ON searchview(tsv);
 CREATE EXTENSION if not exists pg_trgm;
