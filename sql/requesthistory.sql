324246c089b26e66a3f54134588900a1111f5e2b
skjaldbaka17 <skjaldbaka17@gmail.com>
2021-06-30 22:08:40 +0000

    Minor fix for one table

--- sql/requesthistory.sql
+++ sql/requesthistory.sql
@@ -4,5 +4,6 @@ CREATE TABLE requesthistory (
     api_key varchar not null,
     route varchar not null,
     request_body text not null,
+    request text not null,
     created_at timestamptz default current_timestamp
 )
--- sql/useful.sql
+++ sql/useful.sql
@@ -5,5 +5,6 @@ WHERE similarity(word, 'nietshe') > 0.3
 ORDER BY word <-> 'nietshe'
 LIMIT 3;
 
+---To refresh the view after an update 
 REFRESH MATERIALIZED VIEW unique_lexeme;
 REFRESH MATERIALIZED VIEW searchview;
